608c61fa25344f2606b3894aa842d566ef4374c2
skrskrm <50164945+skrskrm@users.noreply.github.com>
2021-02-13 23:52:22 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 // Script.js
-
 window.addEventListener('DOMContentLoaded', () => {
   function fetchData() {
     fetch('https://fakestoreapi.com/products')
@@ -11,24 +10,24 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   let array_data = JSON.parse(localStorage.getItem("products_data"));
   //console.log(array_data[19].description);
-
+/*
   function updateShadow(elem, titleC) {
     var shadow = elem.shadowRoot;
     var childNodes = shadow.childNodes;
     childNodes[1].getElementsByClassName("title").innerText = "hello";
     console.log(childNodes.children[0].innerHTML);
-    /*
+
     for(var i = 0; i < childNodes.length; i++) {
       if(childNodes[i].nodeName === 'title') {
         childNodes[i].textContent = titleC;
       }
     }
-    */
-  }
 
+  }
+*/
   for(var i = 0; i<20; i++) {
     var newItem = document.createElement("product-item");
-    updateShadow(newItem, array_data[i].title);
+    //updateShadow(newItem, array_data[i].title);
     document.getElementById("product-list").appendChild(newItem);
     //console.log(newItem);
   }
