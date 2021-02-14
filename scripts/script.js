3699329fb1a008235220b3a6ad29a453b3550a7d
skrskrm <50164945+skrskrm@users.noreply.github.com>
2021-02-14 00:20:00 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -9,27 +9,41 @@ window.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem("products_data", JSON.stringify(data));
   }
   let array_data = JSON.parse(localStorage.getItem("products_data"));
-  //console.log(array_data[19].description);
-/*
-  function updateShadow(elem, titleC) {
-    var shadow = elem.shadowRoot;
-    var childNodes = shadow.childNodes;
-    childNodes[1].getElementsByClassName("title").innerText = "hello";
-    console.log(childNodes.children[0].innerHTML);
+  //console.log(array_data[i].title);
 
-    for(var i = 0; i < childNodes.length; i++) {
-      if(childNodes[i].nodeName === 'title') {
-        childNodes[i].textContent = titleC;
-      }
-    }
-
-  }
-*/
   for(var i = 0; i<20; i++) {
     var newItem = document.createElement("product-item");
-    //updateShadow(newItem, array_data[i].title);
+
+    const product = document.createElement('li');
+    product.setAttribute('class','product');
+
+    const img = document.createElement('img');
+    img.width = 200;
+    img.src = array_data[i].image;
+    img.alt = array_data[i].title;
+
+    const title = document.createElement('p');
+    title.setAttribute('class','title');
+    title.innerText = array_data[i].title;
+
+    const price = document.createElement('p');
+    price.setAttribute('class','price');
+    price.innerText = array_data[i].price;
+
+    const addCartBtn = document.createElement('button');
+    addCartBtn.innerText = "Add to Cart";
+    addCartBtn.onclick = function() {
+      alert('Added to Cart!');
+      document.getElementById('cart-count').innerText++;
+    };
+
+    product.appendChild(img);
+    product.appendChild(title);
+    product.appendChild(price);
+    product.appendChild(addCartBtn);
+    newItem.shadowRoot.appendChild(product);
+    
     document.getElementById("product-list").appendChild(newItem);
-    //console.log(newItem);
   }
 
 });
