66cce80f26ff12bcd5d952e80a569735f8ff08f8
skrskrm <50164945+skrskrm@users.noreply.github.com>
2021-02-16 16:08:43 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -26,7 +26,7 @@ window.addEventListener('DOMContentLoaded', () => {
   async function fetchData() {
     let response = await fetch('https://fakestoreapi.com/products');
     let data = await response.json();
-    storeLocal(data);
+    await storeLocal(data);
     /*
     fetch('https://fakestoreapi.com/products')
     .then( response => response.json() )
